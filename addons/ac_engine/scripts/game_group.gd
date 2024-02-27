cf5163e31bd4327e940fa6694e08f204b895d24d
FirowMD <georkuchiev@mail.ru>
2024-02-27 06:07:03 +0300

    Fixed group_type of AcGameGroup. Now its enum instead of int

--- addons/ac_engine/scripts/game_group.gd
+++ addons/ac_engine/scripts/game_group.gd
@@ -3,7 +3,7 @@ extends Node
 
 
 @export var group_name: String = "group_name"
-@export var group_type: int = AcTypes.GameGroupType.NEUTRAL
+@export var group_type: AcTypes.GameGroupType = AcTypes.GameGroupType.NEUTRAL
 @export var group_color: Color = Color(1, 1, 1, 1)
 
 
@@ -11,7 +11,7 @@ func get_group_name() -> String:
 	return group_name
 
 
-func get_group_type() -> int:
+func get_group_type() -> AcTypes.GameGroupType:
 	return group_type
 
 

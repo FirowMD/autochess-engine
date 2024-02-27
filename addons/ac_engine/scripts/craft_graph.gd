b68e4cb175d17603c039c8ad5181cb3e46532662
FirowMD <georkuchiev@mail.ru>
2024-02-27 05:22:20 +0300

    Added CraftSystem

---
+++ addons/ac_engine/scripts/craft_graph.gd
@@ -0,0 +1,133 @@
+class_name AcCraftGraph
+extends Node
+## An abstract class that provides a graph data structure for crafting recipes
+## AcCraftSystem must has a reference to this class to work properly 
+
+
+var node_template: Dictionary = {
+	"data": null,
+	"parents": [], # List of Dictionaries
+	"children": [], # List of Dictionaries
+}
+
+## Nodes that have no parents
+var root_nodes: Array[Dictionary] = []
+
+
+func add_node(data: Variant, parents: Array[Variant] = []) -> Dictionary:
+	var node = node_template.duplicate(true)
+	node["data"] = data
+
+	if parents == []:
+		if root_nodes.find(node) == -1:
+			root_nodes.append(node)
+	else:
+		for parent in parents:
+			var parent_node = search_node(parent)
+			if parent_node["children"].find(node) == -1:
+				parent_node["children"].append(node)
+			
+			if node["parents"].find(parent_node) == -1:
+				node["parents"].append(parent_node)
+
+	return node
+
+
+func search_node_children(node: Dictionary, data: Variant) -> Dictionary:
+	if node["children"] == []:
+		return {}
+	
+	for child in node["children"]:
+		if child["data"] == data:
+			return child
+		
+		if child["children"]:
+			var result = search_node_children(child, data)
+			if result:
+				return result
+
+	return {}
+
+
+func search_node(data: Variant) -> Dictionary:
+	for node in root_nodes:
+		if node["data"] == data:
+			return node
+		
+		var result = search_node_children(node, data)
+		if result:
+			return result
+	
+	return {}
+
+
+func remove_node(data: Variant) -> void:
+	var node = search_node(data)
+	if node:
+		for parent in node["parents"]:
+			parent["children"].erase(node)
+		
+		for child in node["children"]:
+			child["parents"].erase(node)
+		
+		if root_nodes.find(node) != -1:
+			root_nodes.erase(node)
+
+
+func print_node(node: Dictionary, level: int) -> void:
+	var tabs = ""
+	for i in range(level):
+		tabs += "\t"
+	
+	print(tabs + str(node["data"]))
+
+	for child in node["children"]:
+		print_node(child, level + 1)
+
+
+func print_graph() -> void:
+	for node in root_nodes:
+		print_node(node, 0)
+
+
+func to_json_children(node: Dictionary) -> String:
+	var json = "["
+
+	for i in range(node["children"].size()):
+		json += "{\"data\": " + str(node["children"][i]["data"]) + ", \"children\": " + to_json_children(node["children"][i]) + "}"
+		if i < node["children"].size() - 1:
+			json += ", "
+	
+	json += "]"
+	return json
+
+
+func to_json() -> String:
+	var json = "["
+
+	for i in range(root_nodes.size()):
+		json += "{\"data\": " + str(root_nodes[i]["data"]) + ", \"children\": " + to_json_children(root_nodes[i]) + "}"
+		if i < root_nodes.size() - 1:
+			json += ", "
+	
+	json += "]"
+	return json
+
+
+func from_json_children(node: Dictionary, json: Array) -> void:
+	for child in json:
+		var new_node = add_node(child["data"])
+		node["children"].append(new_node)
+		from_json_children(new_node, child["children"])
+
+
+func from_json(json: String) -> void:
+	var json_parser = JSON.new()
+	var result = json_parser.parse(json)
+	if result != OK:
+		print("Error parsing JSON")
+		return
+	
+	for node in json_parser.data:
+		var new_node = add_node(node["data"])
+		from_json_children(new_node, node["children"])
--- addons/ac_engine/scripts/craft_system.gd
+++ addons/ac_engine/scripts/craft_system.gd
@@ -1,5 +1,8 @@
 class_name AcCraftSystem
 extends Node
+## An abstract class that represents a craft system
+## Here you can add items and their parts, and save/load the data to/from a json file
+
 
 @export_group("General")
 ## The path to the json file that contains the craft system initial data
@@ -7,6 +10,37 @@ extends Node
 
 @export_group("Advanced")
 @export var game_controller: AcGameController = null
-@export var ac_tree: AcTree = null
+@export var craft_graph: AcCraftGraph = null
 
-# func add_craft_item(item: AcCombatUnit) -> void:
+
+## Add item, which builds from `parts` (which are parents)
+func add_craft_item(item: Variant, parts: Array[Variant]) -> void:
+	var item_node = craft_graph.add_node(item, parts)
+
+
+## An example of how to add items to the craft system
+func test_data_load():
+	add_craft_item("Ore", [])
+	add_craft_item("Coal", ["Ore"])
+	add_craft_item("Copper", ["Ore"])
+	add_craft_item("Tin", ["Ore"])
+	add_craft_item("Iron", ["Ore"])
+	add_craft_item("Carbon", ["Coal"])
+	add_craft_item("Bronze", ["Copper", "Tin"])
+	add_craft_item("Steel", ["Iron", "Carbon"])
+
+	# # Save json data to file
+	# var file_json = FileAccess.open(json_path, FileAccess.WRITE)
+	# file_json.store_string(craft_graph.to_json())
+
+	# # Load json data from file
+	# var file_json = FileAccess.open(json_path, FileAccess.READ)
+	# var json_data = file_json.get_as_text()
+	# craft_graph.from_json(json_data)
+
+	craft_graph.print_graph()
+
+
+func _ready():
+	test_data_load()
+	pass
